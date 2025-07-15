e0449b2a63160b84771ccd98968d27e80260c4c7
Aditya-Swain <adityaswain817@gmail.com>
2025-07-15 18:41:51 +0530

    project updated

--- debug_token.py
+++ debug_token.py
@@ -1,7 +1,7 @@
 import jwt
 
 # Your token
-token_str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzM0NTAzNjU4LCJpYXQiOjE3MzQ0MTcyNTgsImp0aSI6ImE1NmJiYjc4NDExNzRkM2VhYjc5ODUzMmM1NTQzNjZiIiwidXNlcl9pZCI6MX0.9rZK3ZgHh5JWDHjJR7TtjOwif2oDgtVQbPqJXSKe7NQ"
+token_str = ""
 
 # Replace with your Django secret key
 SECRET_KEY = "django-insecure-mt=7971#o+6h2k*uwmm44oi7)#d9k*#xd=i%=0%@kz4g^d^o0k"
